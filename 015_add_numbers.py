431ff2b611a5963ace22cdea88c548d8fda56d95
i000o <iosman1096@gmail.com>
2025-08-09 13:19:43 +0100

    add numbers function

--- 015_add_numbers.py
+++ 015_add_numbers.py
@@ -13,7 +13,8 @@ from lib.helpers import check_that_these_are_equal
 # YOUR FUNCTION GOES BELOW THIS LINE
 
 def add_numbers(num_a, num_b): 
-  return num_a + num_b
+  return num_a + num_b 
+# indentation matters 
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
 
