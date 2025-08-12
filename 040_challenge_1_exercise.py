a5763ad724ebbb128c5b44870a2181c4942cfc8d
i000o <iosman1096@gmail.com>
2025-08-12 12:01:05 +0100

    writing a function that uses a for loop on a list to create a new list and return a joined string with conditionals

--- 040_challenge_1_exercise.py
+++ 040_challenge_1_exercise.py
@@ -27,8 +27,15 @@ from lib.helpers import check_that_these_are_equal
 print("")
 print("Function: report_long_words")
 
-def report_long_words(words):
-  pass
+def report_long_words(words): # make a function called report_long_words which takes the parameter words 
+  long_words = [] # make a list called long_words 
+  for word in words: # iterate over each word in the words list 
+    if len(word) > 10 and "-" not in word: # and if the length of the word is greater than 10 (characters) AND there is no hyphen in the word
+      if len(word) > 15: # not only this, but if the length of the word is also greater than 15
+        long_words.append(word[:15] + '...') # go into the long_words list, and add the word, but cut it at the 15th character and add an ellipse at the end 
+      else: #otherwise, in the cases where the words are not so long that they need to be manipulated
+        long_words.append(word) # just add them to the long_words list
+  return "These words are quite long: " + ", " .join(long_words) # return me These words are quite long: and include a new string of all the words which are longer than 10 characters, with commas inbetween, and if they're longer than 15, cut them and add ellipse.
 
 check_that_these_are_equal(
   report_long_words([
